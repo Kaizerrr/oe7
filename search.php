791eb3a01cf24d49a8498ed962d74d12ee8d2f9d
Kaizerrr <jmc6740@gmail.com>
2021-12-07 10:11:25 +0800

    first commit

---
+++ search.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+ 
+<title>Search Form</title>
+
+<body>
+    <form method="post" class="search" action="lookup.php" >
+    <h1>Search for Records</h1>
+    <input type="text" name="search" placeholder="Search..."/>
+    <button type="submit" name="submit" value="Search"> submit </button>
+</form>
+</body>
+</html>
